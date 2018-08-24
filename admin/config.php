780258c4d7ec78186306be98cd6b8ca731b789c6
mark-marushak <markk.9964@gmail.com>
2018-08-24 17:15:16 +0300

    config admin/config

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://baradach.loc/admin/');
-define('HTTP_CATALOG', 'http://baradach.loc/');
+define('HTTP_SERVER', 'http://mcshoes.com.ua/admin/');
+define('HTTP_CATALOG', 'http://mcshoes.com.ua/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://baradach.loc/admin/');
-define('HTTPS_CATALOG', 'http://baradach.loc/');
+define('HTTPS_SERVER', 'https://mcshoes.com.ua/admin/');
+define('HTTPS_CATALOG', 'https://mcshoes.com.ua/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/OpenServer/domains/baradach.loc/admin/');
-define('DIR_SYSTEM', 'D:/OpenServer/domains/baradach.loc/system/');
-define('DIR_IMAGE', 'D:/OpenServer/domains/baradach.loc/image/');
-define('DIR_LANGUAGE', 'D:/OpenServer/domains/baradach.loc/admin/language/');
-define('DIR_TEMPLATE', 'D:/OpenServer/domains/baradach.loc/admin/view/template/');
-define('DIR_CONFIG', 'D:/OpenServer/domains/baradach.loc/system/config/');
-define('DIR_CACHE', 'D:/OpenServer/domains/baradach.loc/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/OpenServer/domains/baradach.loc/system/storage/download/');
-define('DIR_LOGS', 'D:/OpenServer/domains/baradach.loc/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/OpenServer/domains/baradach.loc/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/OpenServer/domains/baradach.loc/system/storage/upload/');
-define('DIR_CATALOG', 'D:/OpenServer/domains/baradach.loc/catalog/');
+define('DIR_APPLICATION', 'http://mcshoes.com.ua/admin/');
+define('DIR_SYSTEM', 'http://mcshoes.com.ua/system/');
+define('DIR_IMAGE', 'http://mcshoes.com.ua/image/');
+define('DIR_LANGUAGE', 'http://mcshoes.com.ua/admin/language/');
+define('DIR_TEMPLATE', 'http://mcshoes.com.ua/admin/view/template/');
+define('DIR_CONFIG', 'http://mcshoes.com.ua/system/config/');
+define('DIR_CACHE', 'http://mcshoes.com.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', 'http://mcshoes.com.ua/system/storage/download/');
+define('DIR_LOGS', 'http://mcshoes.com.ua/system/storage/logs/');
+define('DIR_MODIFICATION', 'http://mcshoes.com.ua/system/storage/modification/');
+define('DIR_UPLOAD', 'http://mcshoes.com.ua/system/storage/upload/');
+define('DIR_CATALOG', 'http://mcshoes.com.ua/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'baradach');
+define('DB_HOSTNAME', '193.169.188.252');
+define('DB_USERNAME', 'mcshoesc_admin');
+define('DB_PASSWORD', '71D9RMM51IIJ');
+define('DB_DATABASE', 'mcshoesc_shop');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
