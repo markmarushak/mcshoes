103d92813e0adac32e2558f3ac1de84c859380c1
mark-marushak <markk.9964@gmail.com>
2018-08-25 12:42:35 +0300

    branch_18

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://mcshoes.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://mcshoes.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/mcshoesc/public_html/catalog/');
+define('DIR_SYSTEM', '/home/mcshoesc/public_html/system/');
+define('DIR_IMAGE', '/home/mcshoesc/public_html/image/');
+define('DIR_LANGUAGE', '/home/mcshoesc/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/mcshoesc/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/mcshoesc/public_html/system/config/');
+define('DIR_CACHE', '/home/mcshoesc/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/mcshoesc/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/mcshoesc/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/mcshoesc/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/mcshoesc/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '193.169.188.252');
+define('DB_USERNAME', 'mcshoesc_admin');
+define('DB_PASSWORD', 'ubXQjDkq38If');
+define('DB_DATABASE', 'mcshoesc_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
